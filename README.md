b93a77e3e1390b86a9d3f3634e65d12ab18941ad
Yann Isabel <isabel.yann@gmail.com>
2016-06-16 23:15:31 +0200

    add all directory gulp workflow

---
+++ README.md
@@ -0,0 +1,25 @@
+## 1\. Getting Started
+
+First you need to install [Node.js](https://nodejs.org/en/ "officiel Nodejs website") on your computer. Then open a terminal (Your favorite terminal will be perfect), and now go to the directory base with the following command:
+
+`cd path/to/the/directory/gulp-workflow`
+
+
+## 2\. Installation
+
+Here, you can install [NPM](https://www.npmjs.com/ "npmjs official website")(Node Package Manager) dependancies.
+
+Run this command:
+
+`npm install`
+
+It installs all dependancies found in the `package.json`.
+
+
+## 3\. Run it
+
+Just run the following command line:
+`gulp`
+
+It execute the default task and launch the server.
+I think it's better to show you this workflow in action. So the `index.html` is a kind of documentation and it can help you with some option.
---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var gulp = require('gulp'),
+    /*You can see dependancies in the package.json */
+    plugins = require('gulp-load-plugins')({
+        pattern: '*'
+    }),
+    browserSync = require('browser-sync');
+
+/* Paths to Dev and web environnement for path flexibility*/
+var devPaths = {
+    base: 'src',
+    tmpl: 'src/templates',
+    styles: 'src/sass',
+    script: 'src/js',
+    img: 'src/images',
+    fonts: 'src/fonts'
+};
+
+var webPaths = {
+    tmpl: 'web',
+    styles: 'web/css',
+    script: 'web/js',
+    img: 'web/images',
+    fonts: 'web/fonts'
+};
+
+
+// Browser synchronisation
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+          baseDir: webPaths.tmpl
+        },
+    });
+});
+
+// Duplicate fonts folder in webPath
+gulp.task('fonts', function() {
+  return gulp.src(devPaths.fonts + '/**/*')
+  .pipe(gulp.dest(webPaths.fonts));
+});
+
+// Duplicate images folder in webPath and minify them
+gulp.task('imagemin', function(){
+    return gulp.src(devPaths.img + '/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(plugins.cache(plugins.imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest(webPaths.img));
+});
+
+// Compile jade templates to html pages
+gulp.task('jade', function() {
+    return gulp.src(devPaths.tmpl + '/**/*.jade')
+        .pipe(plugins.jade({
+		}))
+        .pipe(plugins.prettify({indent_size: 4, preserve_newlines: true }))
+        .pipe(gulp.dest(webPaths.tmpl))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Duplicate main.js file and minify it in the webPath
+gulp.task('js', function() {
+    return gulp.src(devPaths.script + '/**/*.js')
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(webPaths.script))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Compile sass main file to a css file in webPath
+gulp.task('sass', function() {
+    return gulp.src(devPaths.styles + '/main.scss')
+        .pipe(plugins.sass({
+            precision: 10,
+            onError: console.error.bind(console, 'Sass error:')
+        }))
+        .pipe(plugins.cssbeautify({
+            indent: '  ',
+            autosemicolon: true
+        }))
+        .pipe(gulp.dest(webPaths.styles))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+// Duplicate css files to the webPath
+gulp.task('css', function() {
+    return gulp.src(devPaths.styles + '/**/*.css')
+        .pipe(gulp.dest(webPaths.styles))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Default Task
+gulp.task('default', ['browserSync', 'fonts', 'imagemin', 'jade', 'js', 'sass', 'css'], function() {
+    gulp.watch(devPaths.fonts + '**/*', ['fonts']);
+    gulp.watch(devPaths.img + '/**/*.+(png|jpg|jpeg|gif|svg)', ['imagemin']);
+    gulp.watch(devPaths.tmpl + '/**/*.jade', ['jade']);
+    gulp.watch(devPaths.script + '/**/*.js', ['js']);
+    gulp.watch(devPaths.styles + '/**/*.scss', ['sass']);
+    gulp.watch(devPaths.styles + '/**/*.css', ['css']);
+
+});
